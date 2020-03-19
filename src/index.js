e659a4b1e197c6e8d6da34c1ee509d8dabf25371
prabu2930 <31720576+prabu2930@users.noreply.github.com>
2020-03-19 00:16:53 +0000

    Added comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { createButton, createPara } from './dateDisplayLibrary'
 
 import renderDate, { renderDay } from './displayDate'
 
+
 function renderApp(fun) {
+
+    //'createButton' will return addEventListner function to attach listner.
     var addButtonDateEventListner = createButton('Display Date');
     addButtonDateEventListner(renderDate);
     var addButtonDayEventListner = createButton('Display Day');
